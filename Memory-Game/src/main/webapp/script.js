735d2cd5dcbbc2e739876736c3e96aaa7f1db9fe
Tanvi Ohri <tanviohri@gmail.com>
2020-08-15 05:48:42 +0000

    Implement non-collaborative, basic version of game

--- Memory-Game/src/main/webapp/script.js
+++ Memory-Game/src/main/webapp/script.js
@@ -15,4 +15,125 @@
 /**
  * Adds a random greeting to the page.
  */
+
+ // All usefull objects and shortcuts to simplify coding
+let objects = ['bug', 'bug', 'bicycle', 'bicycle', 'leaf', 'leaf', 'cube', 'cube', 'anchor', 'anchor', 'paper-plane-o', 'paper-plane-o', 'bolt', 'bolt', 'bomb', 'bomb', 'diamond', 'diamond'],
+
+    // Useful selectors shortened
+    $container = $('.container'),
+    $scorePanel = $('.score-panel'),
+    $scoreRed = $('.score-red'),
+    $turn = $('.turn'),
+    $scoreBlue = $('.score-blue'),
+    $deck = $('.deck'),
+
+    // Set variables to shorten code
+    allOpen = [],
+    matchRed = 0,
+    matchBlue=0;
+    wait = 420,
+    totalCard = objects.length / 2;
+
+// Shuffling function: enables that no two games have the same card arrangement 
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
+
+// The function init() enables the game to begin
+function init() {
+
+    // The shuffle function shuffles the objects array
+    let allCards = shuffle(objects);
+    $deck.empty();
+
+    // The game starts with no matching cards
+    matchRed = 0;
+    matchBlue=0;
+    turn="Red";
+    $scoreRed.html(matchRed);
+    $scoreBlue.html(matchBlue);
+    $turn.html(turn);
+
+    // A for loop creates <li> tags with the class of card for every <i> tag
+    // A class of fa fa- and a name of each object from the objects=[] array
+    for (let i = 0; i < allCards.length; i++) {
+        $deck.append($('<li class="card"><i class="fa fa-' + allCards[i] + '"></i></li>'))
+    }
+    addCardListener();
+}
+
+// Add boostrap modal alert window showing time, moves, score it took to finish the game, toggles when all pairs are matched.
+function gameOver(team) {
+    $('#winnerText').text(`${team} Team wins! Well done!`);
+    $('#winnerModal').modal('toggle');
+}
+
+// This function allows each card to be validated that is an equal match to another card that is clicked on to stay open.
+// If cards do not match, both cards are flipped back over.
+let addCardListener = function () {
+
+    // With the following, the card that is clicked on is flipped
+    $deck.find('.card').bind('click', function () {
+        let $this = $(this);
+        if ($this.hasClass('open') || $this.hasClass('match')) { return true; }
+        let card = $this.context.innerHTML;
+        //console.log(card); this gives <i class="fa fa-cube"></i>
+        $this.addClass('open');
+        allOpen.push(card);
+
+        // Compares cards if they matched
+        if (allOpen.length > 1) {
+            if (card === allOpen[0]) {
+                $deck.find('.open').addClass('match');
+                setTimeout(function () {
+                    $deck.find('open').removeClass('open');
+                }, wait);
+                if(turn=="Red")
+                {
+                    matchRed++;                    
+                    $scoreRed.html(matchRed);
+                }
+                else if(turn=="Blue") 
+                {
+                    matchBlue++;                    
+                    $scoreBlue.html(matchBlue);
+                }
+                // If cards are not matched, there is a delay of 630ms, and the cards will turn back cover up.
+            } else {
+                setTimeout(function () {
+                    $deck.find('.open').removeClass('open');
+                }, wait / 1.5);
+            }
+
+            // The allOpen array specifies all added cards facing up
+            allOpen = [];
+            if(turn=="Red") turn="Blue";
+            else if(turn=="Blue") turn="Red";
+            $turn.html(turn);
+        }
+
+        // The game is finished once all cards have been matched, with a short delay
+        if (matchRed > totalCard/2) {
+            setTimeout(function () {
+                gameOver("Red");
+            }, 500);
+        }
+        else if(matchBlue > totalCard/2){
+            setTimeout(function () {
+                gameOver("Blue");
+            }, 500);
+        }
+    });
+}
+
+init();
  
