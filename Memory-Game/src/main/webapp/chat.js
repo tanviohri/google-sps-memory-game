9108f1a015d4fd8e416120c5ba8ff70ed783f349
Tanvi Ohri <tanviohri@gmail.com>
2020-08-18 18:29:51 +0000

    Add container to store chat

--- Memory-Game/src/main/webapp/chat.js
+++ Memory-Game/src/main/webapp/chat.js
@@ -1,3 +1,34 @@
+window.customElements.define('speech-bubble', class extends HTMLElement {'p'});
+
+async function getMessagesForTeamChat() {
+    //const response = await fetch('/data'); --to implement
+    //const messages = await response.json(); --to implement
+    container=document.getElementById('team-chat-container');
+    messages.map(function(currMessage){
+        container.appendChild(
+            createElement(currMessage)
+        );
+    });
+}
+
+async function getMessagesForRoomChat() {
+    //const response = await fetch('/data'); --to implement
+    //const messages = await response.json(); --to implement
+    container=document.getElementById('room-chat-container');
+    messages.map(function(currMessage){
+        container.appendChild(
+            createElement(currMessage)
+        );
+    });
+}
+
+function createElement(currMessage) {
+  const element = document.createElement('speech-bubble');
+  element.innerHTML = "<b>" + currMessage.teamMember.nickname + "</b> <br>" + currMessage.text;
+
+  return element;
+}
+
 function openForm(chatType) {
     document.getElementById(chatType + "Chat").style.display = "block";
 }
