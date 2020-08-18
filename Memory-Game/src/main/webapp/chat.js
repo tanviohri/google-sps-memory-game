80401a7b8eab1a7b813cbdfe408013e9b750d64d
Tanvi Ohri <tanviohri@gmail.com>
2020-08-18 14:46:53 +0000

    Add basic chat UI

---
+++ Memory-Game/src/main/webapp/chat.js
@@ -0,0 +1,7 @@
+function openForm(chatType) {
+    document.getElementById(chatType + "Chat").style.display = "block";
+}
+
+function closeForm(chatType) {
+    document.getElementById(chatType + "Chat").style.display = "none";
+}
