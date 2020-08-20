ef83fc4d0511f312384dfe6432b21b7bc2d31c11
Ashish Agarwal <ashish01012000@gmailcom>
2020-08-20 17:53:38 +0000

    Add chat servlet

---
+++ Memory-Game/src/main/java/com/google/sps/servlets/TeamChat.java
@@ -0,0 +1,34 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.lang.*;
+import org.json.simple.*;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+import com.google.sps.data.*;
+import static com.google.sps.util.Util.*;
+
+import com.google.appengine.api.users.*;
+
+@WebServlet("team-chat")
+public class TeamChat extends HttpServlet{
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+
+        JSONObject obj = getJsonObjectFromRequest(request);
+        long inviteCode = (long) obj.get("inviteCode");
+        String text = (String) obj.get("message");
+
+        Game game = ofy().load().type(Game.class).id(inviteCode).now();
+        User user = UserServiceFactory.getUserService().getCurrentUser();
+        TeamMember teamMember = game.getTeamMemberFromUser(user);
+
+        game.addMessageInTeamChat(new Message(teamMember, System.currentTimeMillis(), text));
+        ofy().save().entity(game).now();
+    }
+
+}
