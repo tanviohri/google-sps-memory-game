8d9b7a6780cfce70c1a73dd91ede52bee5e88ece
Tanvi Ohri <tanviohri@gmail.com>
2020-08-23 19:01:32 +0000

    Add functionality to show player's team

--- Memory-Game/src/main/webapp/game.js
+++ Memory-Game/src/main/webapp/game.js
@@ -14,6 +14,7 @@ function init() {
     n=board.length;
     m=board[0].length;
 
+    document.getElementById("your-team").innerHTML = sessionStorage.getItem("yourTeam");
     $turn.html(sessionStorage.getItem("chance"));
 
     console.log("n: " + n + " m: " + m);
